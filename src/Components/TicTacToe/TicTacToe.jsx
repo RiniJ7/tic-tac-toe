8f4bb23c7fd2d467a3ac4441ccea4abbf6914373
rinij <rinimani7@gmail.com>
2024-02-10 14:13:26 -0700

    logic for alternating "x" and "o" implemented

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import "./TicTacToe.css";
 import circle_icon from "../Assets/circle.png";
 import cross_icon from "../Assets/cross.png";
@@ -9,6 +10,30 @@ let data = ["", "","", "","", "","", "",""];
 
 
 const TicTacToe = () => {
+
+let [count, setCount] = useState(0);
+//for locking in after a win
+let [lock, setLock] = useState(false);
+//defining function for toggle functionality when clicking the button
+//input parameters are element and number
+const toggle =(e,num) => {
+  //if the lock is true , exit from the function
+if(lock) {
+  return 0;
+}
+if(count%2 === 0)
+{
+  e.target.innerHTML = `<img src='${cross_icon}'>`;
+  data[num]="x";
+  setCount(++count);
+}
+else {
+  e.target.innerHTML = `<img src='${circle_icon}'>`;
+  data[num]="o";
+  setCount(++count);
+}
+}
+
   return (
     <div className="container">
       <h1 className="title">
@@ -17,19 +42,19 @@ const TicTacToe = () => {
       {/* TicTacToe board */}
       <div className="board">
         <div className="row1">
-          <div className="boxes"></div>
-          <div className="boxes"></div>
-          <div className="boxes"></div>
+          <div className="boxes" onClick={(e)=>{toggle(e,0)}}></div>
+          <div className="boxes" onClick={(e)=>{toggle(e,1)}}></div>
+          <div className="boxes" onClick={(e)=>{toggle(e,2)}}></div>
         </div>
         <div className="row2">
-          <div className="boxes"></div>
-          <div className="boxes"></div>
-          <div className="boxes"></div>
+          <div className="boxes" onClick={(e)=>{toggle(e,3)}}></div>
+          <div className="boxes" onClick={(e)=>{toggle(e,4)}}></div>
+          <div className="boxes" onClick={(e)=>{toggle(e,5)}}></div>
         </div>
         <div className="row3">
-          <div className="boxes"></div>
-          <div className="boxes"></div>
-          <div className="boxes"></div>
+          <div className="boxes" onClick={(e)=>{toggle(e,6)}}></div>
+          <div className="boxes" onClick={(e)=>{toggle(e,7)}}></div>
+          <div className="boxes" onClick={(e)=>{toggle(e,8)}}></div>
         </div>
       </div>
       <button className="reset">Reset</button>
